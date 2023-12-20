0bd550f6b557221c80ad985dcd31fed35e4a843d
Sydney Jeffries <sydney.jeffriess@gmail.com>
2023-12-19 20:37:59 -0600

    get swagger to work.

--- Pharma_Project_2023.Server/Program.cs
+++ Pharma_Project_2023.Server/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 string connectionString = builder.Configuration.GetConnectionString("AppSettingsDbContext");
 
 builder.Services.AddDbContext<AppSettingsDbContext>(options =>
