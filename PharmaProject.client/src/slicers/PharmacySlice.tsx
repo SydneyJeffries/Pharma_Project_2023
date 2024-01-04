27351beead991d9810f5442e9b34875994befdc2
Sydney Jeffries <sydney.jeffriess@gmail.com>
2024-01-03 20:15:09 -0600

    add pagination

--- PharmaProject.client/src/slicers/PharmacySlice.tsx
+++ PharmaProject.client/src/slicers/PharmacySlice.tsx
@@ -11,6 +11,7 @@ const initialState: ISharedState = {
     data: [],
     status: 'idle',
     error: '',
+    totalRowsForPagination: 0,
 };
 
 export const fetchPharmacyList = createAsyncThunk<IPharmacy[]>('pharmacy/fetchList', async (_, { signal }) => {
