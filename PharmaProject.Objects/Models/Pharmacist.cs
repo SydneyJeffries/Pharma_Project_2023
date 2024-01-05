c6f9ff3ea46aeedba559bf8db004d06fe6ace509
Sydney Jeffries <sydney.jeffriess@gmail.com>
2024-01-04 22:24:42 -0600

    update tag

--- PharmaProject.Objects/Models/Pharmacist.cs
+++ PharmaProject.Objects/Models/Pharmacist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +20,9 @@ namespace PharmaProject.Objects.Models
         public DateTimeOffset CreatedDate { get; set; }
         public string CreatedBy { get; set; } = string.Empty;
         public DateTimeOffset? UpdatedDate { get; set; }
-        public string? UpdatedBy { get; set; } 
+        public string? UpdatedBy { get; set; }
 
+        [NotMapped]
         public string FullName { get { return FirstName + ' ' +  LastName; } }
 
       
