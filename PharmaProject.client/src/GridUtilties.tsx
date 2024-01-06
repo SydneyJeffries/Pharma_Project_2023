b12213576cb48f3dcfed1d9536c19ea4494ed3e7
Sydney Jeffries <sydney.jeffriess@gmail.com>
2024-01-05 22:20:54 -0600

    fix case when trying to save and no fields have been added

--- PharmaProject.client/src/GridUtilties.tsx
+++ PharmaProject.client/src/GridUtilties.tsx
@@ -1,41 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {  GridRowId, GridRowModesModel, GridRowModes } from '@mui/x-data-grid';
+import { GridRowId, GridRowModesModel, GridRowModes } from '@mui/x-data-grid';
 
-export const handleEditClick = (   rowModesModel: GridRowModesModel, setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>, GridRowModes: any) => (id: GridRowId) => {
+export const handleEditClick = (rowModesModel: GridRowModesModel, setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>, GridRowModes: any) => (id: GridRowId) => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
 };
 
-export const handleSaveClick = (rowModesModel: GridRowModesModel,setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>,validationErrorsRef: React.MutableRefObject<{ [key: string]: { [key: string]: boolean } }>) => (id: GridRowId) => {
+export const handleSaveClick = (rowModesModel: GridRowModesModel, setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>, validationErrorsRef: React.MutableRefObject<{ [key: string]: { [key: string]: boolean } }>) => (id: GridRowId) => {
     const rowValidationErrors = validationErrorsRef.current[id];
-        if (rowValidationErrors === undefined && id != 0) {
-            // if there are no validation errors on an update
-            setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-        } else {
-            const hasRowError = Object.values(rowValidationErrors).filter((hasError) => hasError === true);
-            
-            if (hasRowError.length === 0) {
-                // can return to view bc no errors
-                setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-            } else {
-                // highlight the columns with validation errors 
-                document.querySelectorAll('.Mui-error').forEach(function (element) {
-                    element.classList.add('validation-ran');
-                });
-            }
-        }
+
+    if (rowValidationErrors === undefined && id != 0) {
+        // if there are no validation errors on an update
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+        return;
+    }
+    if (rowValidationErrors === undefined) {
+        // if no rows have been clicked on an new record and user tried to save
+        document.querySelectorAll('.Mui-error').forEach(function (element) {
+            element.classList.add('validation-ran');
+        });
+        return;
+    }
+
+    const hasRowError = Object.values(rowValidationErrors).filter((hasError) => hasError === true);
+
+    if (hasRowError.length === 0) {
+        // can return to view bc no errors
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    } else {
+        // highlight the columns with validation errors 
+        document.querySelectorAll('.Mui-error').forEach(function (element) {
+            element.classList.add('validation-ran');
+        });
+    }
+
 };
 
-export const handleCancelClick = (rowModesModel: GridRowModesModel, setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>, rows: any[], setRows: React.Dispatch<React.SetStateAction<any>>, setDeleteDisabled? : any) => (id: GridRowId) => {
-        setRowModesModel({
-            ...rowModesModel,
-            [id]: { mode: GridRowModes.View, ignoreModifications: true },
-        });
-        if (setDeleteDisabled) {
-            setDeleteDisabled(false)
-        }
-        const editedRow = rows.find((row) => row.id === id);
-        if (editedRow?.isNew) {
-            setRows(rows.filter((row) => row.id !== id));
-        }
+export const handleCancelClick = (rowModesModel: GridRowModesModel, setRowModesModel: React.Dispatch<React.SetStateAction<GridRowModesModel>>, rows: any[], setRows: React.Dispatch<React.SetStateAction<any>>, setDeleteDisabled?: any) => (id: GridRowId) => {
+    setRowModesModel({
+        ...rowModesModel,
+        [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    // if need to disable delete while in add new record mode
+    if (setDeleteDisabled) {
+        setDeleteDisabled(false)
+    }
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow?.isNew) {
+        setRows(rows.filter((row) => row.id !== id));
+    }
 };
 
