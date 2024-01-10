0ba990c860d2465b0d24497bf10dec0ca5948321
Sydney Jeffries <sydney.jeffriess@gmail.com>
2024-01-10 16:31:27 -0600

    get rid of pharmacy drug

--- PharmaProject.Objects/Models/Pharmacist.cs
+++ PharmaProject.Objects/Models/Pharmacist.cs
@@ -14,7 +14,6 @@ namespace PharmaProject.Objects.Models
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public int Age { get; set; }
-        public int PrimaryDrugId { get; set; }
         public DateTimeOffset DateOfHire { get; set; }
         public bool Active { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
